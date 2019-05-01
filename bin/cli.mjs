1987be6ab43afc04e94d6a1f5d6257986fcafad1
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-05-01 19:34:00 +0100

    fix cli

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -1,6 +1,6 @@
-#!/bin/sh 
+#!/bin/sh
 ":" //# comment; exec /usr/bin/env node --experimental-modules "$0" "$@"
 
-import EsmRunnerCli from '../'
+import EsmRunnerCli from '../index.mjs'
 const cli = new EsmRunnerCli()
 cli.start()
