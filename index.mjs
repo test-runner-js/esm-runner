c216e61c5549743e735505bbaea1f6a0ea88be65
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-11-23 18:15:39 +0000

    fix node v10 test

--- index.mjs
+++ index.mjs
@@ -1,11 +1,14 @@
 import TestRunnerCli from 'test-runner'
-import { createRequire } from 'module'
+import module from 'module'
 
-const require = createRequire(import.meta.url)
+let require
+if (module.createRequire) {
+  require = module.createRequire(import.meta.url)
+}
 
 class EsmRunnerCli extends TestRunnerCli {
   async loadModule (moduleId) {
-    if (moduleId.endsWith('.json')) {
+    if (moduleId.endsWith('.json') && require) {
       return require(moduleId)
     } else {
       return (await import(moduleId)).default
